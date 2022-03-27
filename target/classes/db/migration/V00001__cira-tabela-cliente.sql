762828640ce8c818f18ec1989b79f398a48bf1ac
Tiago Teixeira <tiagoteixeiragbi@gmail.com>
2022-03-27 18:17:34 -0300

    deploy
    inserido parametros de DB postgreeSQL

--- target/classes/db/migration/V00001__cira-tabela-cliente.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists cliente(
-	id bigint not null auto_increment,
-    nome varchar(200) not null,
-    email varchar(255) not null,
-    telefone varchar(20) not null,
-	primary key (id)
-);
